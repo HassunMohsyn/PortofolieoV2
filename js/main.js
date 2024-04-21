759d6e3e2de392dd7536ac3ba99ca83ea408189a
HassunMohsen <hassun9992@gmail.com>
2024-04-21 21:58:38 +0200

    uploud all projects

--- js/main.js
+++ js/main.js
@@ -3,19 +3,16 @@ let app = angular.module("myApp",[]);
 app.controller("myCTR",function ($scope) {
     
     $scope.handleIconClick = function() {
-        console.log('Icon clicked!');
+        // console.log('Icon clicked!');
         let pop = document.querySelector(".projects .pop-up");
         pop.style.display = "block";
         let close = document.querySelector(".pop-up .close_pop")
         close.addEventListener('click', (e)=>{
             pop.style.display = "none";
         })
-
-        let img = document.querySelectorAll(".project img") ;
-        img.forEach(image => {
-            
-        })
-
+        document.querySelector(".over_lay img").forEach( imge => {
+            imge.src = document.querySelector(".pop-up .popUp-Gallery .gallery img").src;
+        });
     };
     $scope.skills=[
         {name:'html5', color:'#e44d26'},
@@ -26,14 +23,18 @@ app.controller("myCTR",function ($scope) {
         ];
     
     $scope.projects=[
-        {name:'ali_cafe',des:{title:'System Coffee',content:'1'},popUp:{Repo:'#',Web:'#'}},
-        {name:'apple',des:{title:'Copy an apple Wep site',content:'2'},popUp:{Repo:'#',Web:'#'}},
-        {name:'dash_bord',des:{title:"",content:"3"},popUp:{Repo:'#',Web:'#'}},
-        {name:'elograa',des:{title:"",content:"4"},popUp:{Repo:'#',Web:'#'}},
-        {name:'glass_animatoin',des:{title:"",content:"5"},popUp:{Repo:'#',Web:'#'}},
-        {name:'moves',des:{title:"",content:"6"},popUp:{Repo:'#',Web:'#'}},
-        {name:'my_page',des:{title:"",content:"7"},popUp:{Repo:'#',Web:'#'}},
-        {name:'tamplate1',des:{title:"",content:"8"},popUp:{Repo:'#',Web:'#'}}]
+        {name:'ali_cafe',link:"https://hassunmohsyn.github.io/SystemCoffee/",des:{title:'System Coffee',content:'1'},popUp:{Repo:'#',Web:'#'}},
+        {name:'apple',link:"https://hassunmohsyn.github.io/Apple-WebSite/#",des:{title:'Copy an apple Wep site',content:'2'},popUp:{Repo:'#',Web:'#'}},
+        {name:'dash_bord',link:"https://hassunmohsyn.github.io/Dash-Board/",des:{title:"",content:"3"},popUp:{Repo:'#',Web:'#'}},
+        {name:'glass_animatoin',link:"https://hassunmohsyn.github.io/Glass/",des:{title:"",content:"5"},popUp:{Repo:'#',Web:'#'}},
+        {name:'moves',link:"",des:{title:"",content:"6"},popUp:{Repo:'#',Web:'#'}},
+        {name:'my_page',link:"https://hassunmohsyn.github.io/My-First-Protofoleo/",des:{title:"",content:"7"},popUp:{Repo:'#',Web:'#'}},
+        {name:'tamplate1',link:"https://hassunmohsyn.github.io/TamplateElzero/",des:{title:"",content:"8"},popUp:{Repo:'#',Web:'#'}}]
+
+        
+        // let img = document.querySelector(".pop-up .popUp-Gallery .gallery img");
+
+        // img.src = document.querySelector(".over_lay img").src;
 
     $scope.contacts=[
         {name:'facebook',link:'https://www.facebook.com/hassun2000?mibextid=ZbWKwL', color:'#3B5998'},
@@ -43,6 +44,7 @@ app.controller("myCTR",function ($scope) {
     ]
     
 })
+
 
 
 
